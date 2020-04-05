fa158157585a6e88f2dc30feb0fcb747eca869c5
jesdomtri <jesdomtri@gmail.com>
2020-04-05 13:34:59 +0200

    Mejoras visuales

--- app/src/main/java/com/example/mimimi/MainActivity.java
+++ app/src/main/java/com/example/mimimi/MainActivity.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
@@ -26,6 +27,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+
         setContentView(R.layout.activity_main);
 
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
